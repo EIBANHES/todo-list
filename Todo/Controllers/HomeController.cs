273813d2da7aaa56ebcab8064af216939cef8f3e
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-05-06 13:46:27 -0400

    Crud do Todo

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
@@ -8,9 +10,50 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get() 
+        public List<TodoModel> Get([FromServices]AppDbContext context) // injeçao de dependencia, usa o context como serviço
         {
-            return "Hello World";
+            return context.Todos.ToList();
         }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context) // injeçao de dependencia, usa o context como serviço
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id); // pegando por id 
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context) // injeçao de dependencia, usa o context como serviço
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
+        }        
+        
+        [HttpPut("/{id:int}")]
+        public TodoModel Put([FromRoute] int id, [FromBody] TodoModel todo, [FromServices] AppDbContext context) // injeçao de dependencia, usa o context como serviço
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return model;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Put([FromRoute] int id, [FromServices] AppDbContext context) // injeçao de dependencia, usa o context como serviço
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
+        }
+
     }
 }
--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,6 +1,9 @@
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(); //Adicionando suporte aos controllers da aplicação
+builder.Services.AddDbContext<AppDbContext>(); // Usa como serviço
 
 var app = builder.Build();
 
